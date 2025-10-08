25d87c011557546fe2f8d6e7074f407108578107
Abdul Rahman <abdulraemon06@gmail.com>
2025-10-08 16:59:43 +0530

    Add files via upload

---
+++ Today task.py
@@ -0,0 +1,24 @@
+#task1
+num=15
+if(num%2==0):
+    print("The number is Even")
+else:
+    print("The number is odd")
+
+#task2
+age=20
+if age>=18:
+    print("you can vote");
+else:
+    print("You cannot vote");
+
+#task3
+n=5;
+if n%3==0 and n%5==0:
+    print("fizz,buzz");
+elif n%3==0:
+    print("fizz");
+elif n%5==0:
+    print("buzz");
+else:
+    print("not divisible");
